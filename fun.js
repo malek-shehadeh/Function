a6d72e6d0d150292307ab550e2eeff1bdebcfd84
malekshehadeh <malek.shehadeh96@gmail.com>
2024-06-05 22:43:30 +0300

    firstversion

---
+++ fun.js
@@ -0,0 +1,46 @@
+// Q1
+
+// var str = prompt("please enter");
+
+// function reverse() {
+//   str = str.split("").reverse().join("");
+
+//   return str;
+// }
+
+// console.log(reverse());
+
+// //////////////////////
+// Q2
+
+// function iteretion() {
+// for (let i = 0; i <= 15; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i + " is even");
+//   } else {
+//     console.log(i + " is odd");
+//   }
+// }
+// }
+// iteretion();
+/////////////////////////
+// Q3
+
+function malek(str) {
+  return str.split("").sort().join("");
+}
+console.log(malek("orange"));
+
+//////////////////////////
+// Q4
+
+//////////////////////////
+// Q5
+// function agechecker(age) {
+//   if (age >= 18) {
+//     console.log("the user is adult");
+//   } else {
+//     console.log("the user is minor");
+//   }
+// }
+// agechecker(15);
